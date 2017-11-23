7964831c8a9ec198d741016f73f0603304810d30
João Canoso <jpcanoso@hotmail.com>
2017-11-23 14:55:20 +0000

    commit 2

--- src/Network/ClientFractal.java
+++ src/Network/ClientFractal.java
@@ -5,10 +5,44 @@
  */
 package Network;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
- * @author Canoso
  */
 public class ClientFractal {
-    
+    public static void main(String[] args) throws IOException {
+        
+        // mensagem a ser transmitida
+        Service s = new Service(0,0, 4);
+        
+        for (int i = 0; i < 100; i++) {
+            
+            //ligação do socket ao servidor
+            Socket socket = new Socket("localhost", 10001);
+            // abertura da stream de saída
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            //abertura da stream de entrada
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            zoom = zoom * 0.9;
+            s.setZoom(zoom);
+            
+            // enviar a mensagem
+            out.writeObject(s);
+            out.flush(); //obriga a enviar o pacote
+            // receber a resposta
+            s = (Service) in.readObject();
+            // apresental a resposta
+            ImageUtils.saveImage(s.getData(), "teste"+i+".jpg");
+            //fechar o socket e as streams
+            socket.close();
+            in.close();
+            out.close();
+        
+        }
+    }
 }
--- src/Network/LinkToServer.java
+++ src/Network/LinkToServer.java
@@ -5,6 +5,10 @@
  */
 package Network;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author Rui Barcelos https://github.com/barcelosrui
@@ -22,6 +26,30 @@ public class LinkToServer extends Thread {
         this.fator = fator;
     }
     
-    
+    public void run(){
+        while( service.getZoom() < 1E-16){
+            //ligação do socket ao servidor
+            Socket socket = new Socket("localhost", 10001);
+            // abertura da stream de saída
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            //abertura da stream de entrada
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            zoom = zoom * 0.9;
+            s.setZoom(zoom);
+            
+            // enviar a mensagem
+            out.writeObject(s);
+            out.flush(); //obriga a enviar o pacote
+            // receber a resposta
+            s = (Service) in.readObject();
+            // apresental a resposta
+            ImageUtils.saveImage(s.getData(), "teste"+i+".jpg");
+            //fechar o socket e as streams
+            socket.close();
+            in.close();
+            out.close();
+        }
+    }
     
 }
