34781a525862e88fde8a4edec65b225f4a1ddc89
Rui Barcelos <barcelos.rui@gmail.com>
2017-11-30 16:42:33 +0000

    ALTER Insert Image TO Server

--- src/Network/FractalCalculatorServer.java
+++ src/Network/FractalCalculatorServer.java
@@ -6,22 +6,16 @@
 package Network;
 
 import FractalNovo.FractalThr;
-import static FractalNovo.FractalThr.getFractal;
-import GUI.GUITESTE;
+import GUI.GUIServer;
 import auxiliar.ImgUtils;
-import fractal.FractalImage;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 /**
  *
@@ -31,9 +25,9 @@ public class FractalCalculatorServer extends Thread {
 
     
     int port = 10000;
-    GUITESTE gui;
+    GUIServer gui;
     
-    public FractalCalculatorServer(GUITESTE gui,int port) {
+    public FractalCalculatorServer(GUIServer gui,int port) {
         this.port = port;
         this.gui = gui;
     }
@@ -57,7 +51,6 @@ public class FractalCalculatorServer extends Thread {
                 
 
                 gui.l.setIcon(icon);
-                gui.panelIMG.add(gui.l);
                 s.setData(ImgUtils.ImageToByte(image));
                 
                 
